645509917cb844d4828163d66c22aa957215a986
minjin <bhs3392@naver.com>
2023-04-06 22:09:37 +0900

    검색 추가

--- app/src/main/java/com/example/sw221103/ChatDTO.java
+++ app/src/main/java/com/example/sw221103/ChatDTO.java
@@ -1,30 +1,55 @@
 package com.example.sw221103;
 
 public class ChatDTO {
-
     private String userName;
     private String message;
+    private String imageUrl;
+    private String link="";
+
 
     public ChatDTO() {}
+
     public ChatDTO(String userName, String message) {
         this.userName = userName;
         this.message = message;
+
     }
 
-    public void setUserName(String userName) {
+    public ChatDTO(String userName, String message, String imageUrl,String link) {
         this.userName = userName;
+        this.message = message;
+        this.imageUrl = imageUrl;
+        this.link = link;
+
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+
+    public String getLink() {
+        return link != null ? link : "";
     }
 
     public String getUserName() {
         return userName;
     }
 
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
     public String getMessage() {
         return message;
     }
-}
 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
